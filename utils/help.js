c3ecc64680b51f2ed05773b6ea9de0762baebf52
mauricioadams <mauricio.adams@gmail.com>
2013-10-05 13:44:19 -0300

    Adding help

--- utils/help.js
+++ utils/help.js
@@ -13,7 +13,31 @@ module.exports = {
 				'  [>=|   ---   |=<]',
 				'     \\\__\\\   /__/',
 				"         '-'",
-				'TODO -- Sorry, cant help right now :('
+				'',
+				' Usage: frankjs [url] [options]',
+				'',
+				' FrankJS options:',
+				'',
+				'  -h, --help                    output usage information (You are here)',
+				'  -cs, --custom-script          overwrite default PhantonJS script (phantomjs-scripts folder)',
+				'  -csp, --custom-script-params  specify any additional options for PhantomJS or PhantomJS custom script (https://github.com/ariya/phantomjs)',
+				'  -rt, --report-template        specify jade templates for report (report-templates folder)',
+				'                                for multiple reports use "," (comma-delimited)',
+				'',
+				' Examples:',
+				'  frankjs google.com -rt=dump-json',
+				'  frankjs google.com -rt=dump-json,my-custom-template',
+				'  frankjs google.com -cs=yslow -rt=my-custom-template',
+				'',
+				' Custom templates:',
+				'  Jade templates are located in report-templates folder.',
+				'  To display result object use (frankjs [URL] -rt=dump-json) or check http://frankjs.org',
+				'',
+				' Custom PhantomJS script:',
+				'  PhantomJS scripts are located in phantomjs-scripts folder.',
+				'  Your custom script must return one valid stringified result object so Jade template can do its magic',
+				'  Some result objects can exceed console.log string size, in this case use "txt" as return and save your object in jsontemp.txt',
+				'  For more information on custom scripts check http://frankjs.org'
 			].join('\n'));
 	}
 };
