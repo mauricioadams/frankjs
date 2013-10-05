54c4602959d512aa2ee08b1bf215d37a49df1d55
mauricioadams <mauricio.adams@gmail.com>
2013-10-05 00:54:12 -0300

    Welcome frankjs
    frankjs birthday :)

---
+++ frankjs.js
@@ -0,0 +1,146 @@
+/*
+	   .'`   `""--.._..--""`   `'.
+      /        .-""-"-""-.        \
+      |       /           \       |
+       \,     | .--.-.--. |     ,/
+       (_'-   |` >       `|   -'_)
+       /      | <         |      \
+       |      (__..---..__)      |
+       |     (`|\o_/ \_o/|`)     |
+       |      \(    >    )/      |
+       |    [>=|   ---   |=<]    |
+       |      ,\__\   /__/,      |
+       |.____.|\==='-'===/|.____.|
+        \_____;_\=======/_;_____/
+         |     _)'.===.'(_     |
+         ;     \-._\_/_.-/     ;
+         /\_\_\_\  ()   /_/_/_/\
+         '-.._____.-'-._____..-'
+             |     /`\     |
+             |_    | |    _|
+           _.;____ | | ____;._
+         /`       `| |`       `\
+         '------'--' '--'------'
+	FRANKJS - http://frankjs.org
+	Copyright (c) 2013, Mauricio Adams <mauricio@frankjs.org>.  All rights reserved.
+*/
+(function() {
+	var result, reports, fileName, arg, fileSet = Date.now(), files = [], totalReports, reporFile,
+	childProcess = require('child_process'),
+	fs = require('fs'),
+	path = require('path'),
+	jade = require('jade'),
+	phantomjs = require('phantomjs'),
+	help = require('./utils/help'),
+	binPath = phantomjs.path,
+	defaultScript = true,
+	argSize = process.argv.length,
+	frankArgs = {
+		url : (process.argv[2].indexOf('http') !== 0) ? 'http://'+ process.argv[2] : process.argv[2],
+		isHelp : false,
+		scriptName : 'frankjs-phantomjs-script',
+		sendReport : false,
+		reportTo: false,
+		reports : 'default',
+		scriptParams : ''
+	};
+	
+	//TODO : Support json package load with arguments
+	for (i = 0; i < argSize; i += 1) {
+		arg = process.argv[i].split("=");
+		switch(arg[0]) {
+			case "--help" : 
+			case "-h" : 
+				frankArgs.isHelp = true; 
+				break;
+			
+			case "--custom-script" :
+			case "-cs" :
+				defaultScript = false;
+				frankArgs.scriptName = arg[1];
+				break;
+			
+			case "--report-template" :
+			case "-rt" :
+				frankArgs.reports = arg[1];
+			
+			case "--custom-script-params" :
+			case "-csp" :
+				frankArgs.scriptParams = arg[1];
+				break;
+				
+			default : '';
+		}
+	}
+	
+	if (process.argv.length === 2) {
+		console.log('Usage: node frankjs <some URL> <options> (node frankjs --help to display options)');
+	} else {
+		if(frankArgs.isHelp) {
+			help.show();
+		} else {
+			//some magic
+			
+			//check if script file exists - TODO
+			
+			var childArgs = [
+			  path.join(__dirname, 'phantomjs-scripts', frankArgs.scriptName + '.js'),
+			  frankArgs.url + frankArgs.scriptParams
+			];
+			
+			//run phantomjs script - output should be json stringfy
+			childProcess.execFile(binPath, childArgs, function(err, output, stderr) {				
+				//default script load json object in a temptxt file (json too big for console.log)
+				if(defaultScript) {
+					result = fs.readFileSync('jsonTemp.txt', 'utf8');
+				}
+
+				result = JSON.parse(result);
+				reports = frankArgs.reports.split(",");
+				//Reports
+				totalReports = reports.length;
+				for(i = 0; i < totalReports; i++)
+				{
+					reportFile = 'report-templates/' + reports[i] + '.txt';
+					fileName = 'result-' + reports[i] + '-' + fileSet + '.html';
+					files.push(fileName);
+					
+					var template = fs.readFileSync(reportFile, 'utf8', function (err,data) {
+					  if (err) {
+						return console.log(err);
+					  }
+					  return data;
+					});
+					
+					//parse jade template
+					var fn = jade.compile(template);
+					var htmlOutput = fn({result: result});
+					
+					//add json dump for archive and future comparison
+					htmlOutput += "<!-- "+ JSON.stringify(result) + " --!>";
+					
+					//write files
+					fs.writeFile('report-results/' + fileName, htmlOutput, 'utf8', function (err) {
+						if (err) return console.log(err);
+					});
+				}
+		
+				if(!frankArgs.sendReport) {
+					console.log('Report created with sucess:');
+					console.log(files.join('\n'));
+				}
+				else {
+					/*
+					* TODO - Create send methods - Send files by email / execute bash / move files to ftp 
+					*/
+				}
+				
+				/*
+				 * TODO - Create metrics monitors (and send alarms)
+				 * DOM > limit, pageload > limit, page offline, img files count/size > limit, css size > limit, DOM nodes > limit
+				*/
+			});
+		}	
+	}
+//bye
+}).call(this);
---
+++ phantomjs-scripts/frankjs-phantomjs-script.js
@@ -0,0 +1,142 @@
+/*
+	   .'`   `""--.._..--""`   `'.
+      /        .-""-"-""-.        \
+      |       /           \       |
+       \,     | .--.-.--. |     ,/
+       (_'-   |` >       `|   -'_)
+       /      | <         |      \
+       |      (__..---..__)      |
+       |     (`|\o_/ \_o/|`)     |
+       |      \(    >    )/      |
+       |    [>=|   ---   |=<]    |
+       |      ,\__\   /__/,      |
+       |.____.|\==='-'===/|.____.|
+        \_____;_\=======/_;_____/
+         |     _)'.===.'(_     |
+         ;     \-._\_/_.-/     ;
+         /\_\_\_\  ()   /_/_/_/\
+         '-.._____.-'-._____..-'
+             |     /`\     |
+             |_    | |    _|
+           _.;____ | | ____;._
+         /`       `| |`       `\
+         '------'--' '--'------'
+	FRANKJS - http://frankjs.org
+	Copyright (c) 2013, Mauricio Adams <mauricio@frankjs.org>.  All rights reserved.
+*/
+var page = require('webpage').create(),
+    system = require('system'),
+	fs = require('fs'),
+	result = {},
+    t;
+
+//create result
+result.errors = [];
+result.resources = [];
+	
+//todo
+//search for social tags, opengraph, twitter and important metatags (like keywords and content)
+//check 3rd party resources
+//check each/split type of request (img,js and css)	
+	
+	
+//adds dom ready event after page initialize
+page.onInitialized = function () {
+	
+	page.timingStartTime = page.evaluate(function () {
+		(function () {
+			//domready
+			document.addEventListener('DOMContentLoaded', function(){window.timingDOMContentLoaded = Date.now();}, false);
+			//window.load
+			window.addEventListener('load', function(){window.timingLoad = Date.now();}, false);
+		})();
+		return Date.now();
+	});
+};	
+
+//watch page resources request
+ page.onResourceRequested = function (req) {
+    result.resources[req.id] = {
+            request: req,
+            startReply: null,
+            endReply: null
+        };
+};
+
+page.onResourceReceived = function (res) {
+    if (res.stage === 'start') {
+        result.resources[res.id].startReply = res;
+		result.resources[res.id].loadTime = Date.now();
+		result.resources[res.id].url = res.url;
+		//converts to kb
+		result.resources[res.id].bodySize =  (res.bodySize / 1024).toFixed(2);
+    }
+    
+	if (res.stage === 'end') {
+        result.resources[res.id].endReply = res;
+		result.resources[res.id].loadTime =  result.resources[res.id].loadTime - res.time;
+    }
+};
+
+page.onResourceError = function(resourceError) {	
+	 result.errors.push({ code : resourceError.errorCode, description : resourceError.errorString, url : resourceError.url});
+};
+
+//gets any error on the page
+page.onError = function (msg, trace) {
+	trace.forEach(function(item) {
+        msg += '  ', item.file, ':', item.line;
+    });
+    result.errors.push({ code : '', description : msg, url : ''});
+}
+	
+if (system.args.length === 1) {
+    console.log('Usage: frankjs-phantomjs-script <some URL>');
+    phantom.exit(1);
+} else {
+	var now = new Date();
+    result.startTime = Date.now();
+	result.dateForHumans = now.getMonth() + '/' + now.getDate() + '/' + now.getFullYear() + ' ' + now.getHours() + ':' + now.getMinutes() + ':' + now.getSeconds();
+	result.date = now.toJSON();
+	result.address = system.args[1];
+    page.open(result.address, function (status) {
+		var startTime = result.startTime;
+		var doc;
+		
+        if (status !== 'success') {
+            result.error.push({ code : 1, description : 'unable to connect', url : address});
+        } else {
+			//loading time
+			result.loadTime = Date.now() - result.startTime;
+			
+			//dom ready time
+			page.timingDOMContentLoaded = JSON.parse(page.evaluate(function () {
+                return window.timingDOMContentLoaded;
+            }));
+            result.domReadyTime = page.timingDOMContentLoaded - page.timingStartTime;
+			
+			//window load time
+			page.timingLoad = JSON.parse(page.evaluate(function () {
+                return window.timingLoad;
+            }));
+			result.timingLoad = page.timingLoad;
+			result.windowReadyTime = page.timingLoad - page.timingStartTime;
+			
+			
+			//get total dom
+			result.totalDOMElements = page.evaluate(function () {
+                return document.getElementsByTagName("*").length;
+            });
+        }
+		
+		//json return too long :( , output in a txt
+	   try {
+			fs.write("jsonTemp.txt", JSON.stringify(result), 'w');
+		} catch(e) {
+			console.log('0');
+			phantom.exit();
+		}
+		console.log('1');
+        phantom.exit();
+    });
+}
